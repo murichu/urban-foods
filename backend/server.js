24397c9e78cade928f4ddb53f1c2585c9cda64ef
Jacob Murichu <77874248+murichu@users.noreply.github.com>
2024-08-28 19:44:02 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ import userRouter from "./routes/userRoute.js";
 import "dotenv/config";
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
+import authentication from './routes/authentication';
+import { errorHandler } from './middleware/errorHandler';
+import bodyParser from 'body-parser';
 
 // app config
 const app = express();
@@ -17,6 +20,11 @@ connectDB();
 // middleware
 app.use(express.json());
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//static files to use css
+app.use(express.static('public'));
 
 // api endpoint
 app.use("/api/foods", foodRouter);
@@ -25,11 +33,13 @@ app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
 // Handle M-Pesa callback
-app.use("/api/mpesa", orderRouter);
+app.use('/api', authentication);
 
 app.get("/", (req, res) => {
   res.send("API WORKING");
 });
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server running on port http://localhost:${port}`);
