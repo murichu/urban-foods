ad36f7abe298f9aa256f731a093654530daf5e61
murichu <murichuchichi@gmail.com>
2025-02-16 12:35:51 +0000

    update url admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -11,7 +11,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "http://localhost:4000";
+  const url = "https://urban-foods-backend.vercel.app";
   return (
     <>
       {/* Toast container for notifications */}
